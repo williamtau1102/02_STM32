ab2790598b667f07e29afd6ea582eb10bb624172
williamtau1102 <williamtau1102@gmail.com>
2025-12-31 23:02:30 +0800

    2512312302

---
+++ 00_Code/02_KEY/WARSHIP_KEY_MDK/MDK-ARM/RTE/_WARSHIP_KEY_MDK/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WARSHIP_KEY_MDK' 
+ * Target:  'WARSHIP_KEY_MDK' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
