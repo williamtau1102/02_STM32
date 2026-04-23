42ed2de6eaaf17e085f13980636651949a03233c
williamtau1102 <williamtau1102@gmail.com>
2026-04-23 22:37:03 +0800

    2604232236

--- 00_Code/01_LED/WARSHIP_LED_CMAKE/Src/main.c
+++ 00_Code/01_LED/WARSHIP_LED_CMAKE/Src/main.c
@@ -98,15 +98,15 @@ int main(void)
   {
     HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
-	//HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_RESET);  
+//	  HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_RESET);  
     
-    HAL_Delay(500);
+    HAL_Delay(1000);
 
     HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
-	//HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_SET);  
+//	  HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_SET);  
     
-    HAL_Delay(500);
+    HAL_Delay(1000);
 
     /* USER CODE END WHILE */
 
--- 00_Code/01_LED/WARSHIP_LED_MDK/MDK-ARM/WARSHIP_LED_MDK/startup_stm32f103xe.d
+++
@@ -1,1 +0,0 @@
-warship_led_mdk\startup_stm32f103xe.o: startup_stm32f103xe.s
